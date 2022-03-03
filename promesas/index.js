9d31e160267efe60888a1b9b97e048869e259557
Leidy <lady-2110@hotmail.com>
2022-03-02 20:41:40 -0500

    promesas

--- promesas/index.js
+++ promesas/index.js
@@ -1,3 +1,23 @@
 let promise= new Promise(function(resolve,reject){
-	setTimeout(()=> reject(new error("Error")),2000)
+	// setTimeout(()=> reject(new error("Error")),2000)
+	// setTimeout(()=> resolve("Correcto"),2000)
+	// resolve("Exitoso");
+	// reject(new Error("...."));
+	// setTimeout(()=>resolve("...."));
+	// //consumidores
+	// //then
+	// //catch
+	// //final
+
+	// promise.then(
+	// 	function(result){},
+	// 	function(erro){}
+	// );
+	
+	setTimeout(()=> resolve("Finalizado"),5000);
 });
+
+promise.then(
+	result => alert(result),
+	error => alert(error)
+);
