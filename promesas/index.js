e7f686f55cc499a51575f58d9ae81d1bc239482c
Leidy <lady-2110@hotmail.com>
2022-03-02 20:54:19 -0500

    promesas parte 3

--- promesas/index.js
+++ promesas/index.js
@@ -1,23 +1,29 @@
-let promise= new Promise(function(resolve,reject){
-	// setTimeout(()=> reject(new error("Error")),2000)
-	// setTimeout(()=> resolve("Correcto"),2000)
-	// resolve("Exitoso");
-	// reject(new Error("...."));
-	// setTimeout(()=>resolve("...."));
-	// //consumidores
-	// //then
-	// //catch
-	// //final
+// let promise= new Promise(function(resolve,reject){
+// 	// setTimeout(()=> reject(new error("Error")),2000)
+// 	// setTimeout(()=> resolve("Correcto"),2000)
+// 	// resolve("Exitoso");
+// 	// reject(new Error("...."));
+// 	// setTimeout(()=>resolve("...."));
+// 	// //consumidores
+// 	// //then
+// 	// //catch
+// 	// //final
 
 	// promise.then(
 	// 	function(result){},
-	// 	function(erro){}
+	// 	function(error){}
 	// );
 	
-	setTimeout(()=> resolve("Finalizado"),5000);
-});
+// 	setTimeout(()=> resolve("Finalizado"),5000);
+// });
 
-promise.then(
-	result => alert(result),
-	error => alert(error)
-);
+// promise.then(
+// 	result => alert(result),
+// 	error => alert(error)
+// );
+
+// new Promise((resolve,reject)=>{
+// 	//Hacer algo
+// })
+// .finally(()=> stop loading indicator)
+// .then(result => show result,err=> show error)
